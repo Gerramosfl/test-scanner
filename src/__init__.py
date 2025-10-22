8ca7f9fd25c29e8d24baf9b0f66134147ceccbc1
Gerramosfl <gerramosfl@gmail.com>
2025-10-22 18:07:55 -0300

    __init__.py solved

--- src/__init__.py
+++ src/__init__.py
@@ -1,41 +1,6 @@
-# src/__init__.py
 """
 Test Scanner - Sistema de Calificación Automática
 """
 
 __version__ = "1.0.0"
-__author__ = "Tu Nombre"
-
-
-# src/ui/__init__.py
-"""
-Módulo de interfaz de usuario
-"""
-
-from .main_window import MainWindow
-from .tab_configuration import ConfigurationTab
-from .tab_answer_key import AnswerKeyTab
-from .tab_grading import GradingTab
-
-__all__ = ['MainWindow', 'ConfigurationTab', 'AnswerKeyTab', 'GradingTab']
-
-
-# src/core/__init__.py
-"""
-Módulo de lógica central
-"""
-
-from .grade_calculator import GradeCalculator
-from .excel_handler import ExcelHandler
-
-__all__ = ['GradeCalculator', 'ExcelHandler']
-
-
-# src/utils/__init__.py
-"""
-Módulo de utilidades
-"""
-
-from .constants import *
-
-__all__ = ['constants']
+__author__ = "Gerramosfl"
--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,26 +1,3 @@
-# src/__init__.py
-"""
-Test Scanner - Sistema de Calificación Automática
-"""
-
-__version__ = "1.0.0"
-__author__ = "Tu Nombre"
-
-
-# src/ui/__init__.py
-"""
-Módulo de interfaz de usuario
-"""
-
-from .main_window import MainWindow
-from .tab_configuration import ConfigurationTab
-from .tab_answer_key import AnswerKeyTab
-from .tab_grading import GradingTab
-
-__all__ = ['MainWindow', 'ConfigurationTab', 'AnswerKeyTab', 'GradingTab']
-
-
-# src/core/__init__.py
 """
 Módulo de lógica central
 """
@@ -29,13 +6,3 @@ from .grade_calculator import GradeCalculator
 from .excel_handler import ExcelHandler
 
 __all__ = ['GradeCalculator', 'ExcelHandler']
-
-
-# src/utils/__init__.py
-"""
-Módulo de utilidades
-"""
-
-from .constants import *
-
-__all__ = ['constants']
